d7eb16a4971a8d75235bcad00352a049d96894a4
Karan Khandelwal <karan.khandelwal74@gmail.com>
2024-06-22 19:33:26 +0530

    Chapter 1 Complete

---
+++ 5_lambdaFunctions.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+
+<body>
+    <?php
+        $books = [
+            [
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Philip K. Dick',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2021,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'The Martian',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2024,
+                'purchaseUrl' => 'http://example.com'
+            ],
+        ];
+
+        function filter($items, $fn)
+        {
+            $filteredItmes = [];
+
+            foreach ($items as $item) {
+                if ($fn($items)) {
+                    $filteredBooks[] = $item;
+                }
+            }
+
+            return $filteredItmes;
+        };
+
+        $filteredBooks = filter($book, function($book){
+            return $book['releaseYear'] >= 2000;
+        });
+    ?>
+
+    <ul>
+        <?php foreach ($filteredBooks as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
